a977830db56e98ebb5090ee21c01758949f81a48
migarakavishan <migarakavishan43@gmail.com>
2024-09-13 16:31:37 +0530

    "Added player spawn point to Level-01.tmx and Level-02 updated Level class to load level based on levelName, and added player character loading to PixelAdventure game."

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -11,10 +11,12 @@ class PixelAdventure extends FlameGame {
   late final CameraComponent cam;
 
   @override
-  final world = Level();
+  final world = Level(levelName: 'Level-01');
 
   @override
-  FutureOr<void> onLoad() {
+  FutureOr<void> onLoad() async {
+    await images.loadAllImages();
+
     cam = CameraComponent.withFixedResolution(
         world: world, width: 640, height: 360);
     cam.viewfinder.anchor = Anchor.topLeft;
