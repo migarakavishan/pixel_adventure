da288cc72b2bfbd4cf9ccd014b2fcff5610176bc
migarakavishan <migarakavishan43@gmail.com>
2024-09-14 14:01:00 +0530

    "Updated Pixel Adventure game: changed active file in tiled-session, added player movement and direction, updated level and main game classes, and modified assets and pubspec.yaml."

--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -1,26 +1,83 @@
 import 'dart:async';
-import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flutter/painting.dart';
+import 'package:pixel_adventure/actors/player.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
+  Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joystick;
+  bool showJoystick = true;
 
   @override
-  final world = Level(levelName: 'Level-01');
-
   @override
   FutureOr<void> onLoad() async {
     await images.loadAllImages();
+    final world = Level(player: player, levelName: 'Level-01');
 
     cam = CameraComponent.withFixedResolution(
         world: world, width: 640, height: 360);
     cam.viewfinder.anchor = Anchor.topLeft;
+
     addAll([cam, world]);
+
+    if (showJoystick) {
+      addJoystick();
+    }
+
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (showJoystick) {
+      updateJoystick();
+    }
+
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystick() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
   }
 }
