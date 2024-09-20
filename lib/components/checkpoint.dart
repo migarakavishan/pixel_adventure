8ca6fe91ef80aee0b9936b3be434b9f6ffc3d8c3
migarakavishan <migarakavishan43@gmail.com>
2024-09-20 21:28:12 +0530

    "Added Checkpoint component and updated Level-01.tmx file with new checkpoint objects"

---
+++ lib/components/checkpoint.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: use_super_parameters
+
+import 'dart:async';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
+
+class Checkpoint extends SpriteAnimationComponent
+    with HasGameRef<PixelAdventure> {
+  Checkpoint({
+    position,
+    size,
+  }) : super(position: position, size: size);
+
+  @override
+  FutureOr<void> onLoad() {
+    // debugMode = true;
+    add(RectangleHitbox(
+      position: Vector2(18, 56),
+      size: Vector2(12, 8),
+    ));
+    animation = SpriteAnimation.fromFrameData(
+        game.images
+            .fromCache('Items/Checkpoints/Checkpoint/Checkpoint (No Flag).png'),
+        SpriteAnimationData.sequenced(
+          amount: 1,
+          stepTime: 1,
+          textureSize: Vector2.all(64),
+        ));
+    return super.onLoad();
+  }
+}
