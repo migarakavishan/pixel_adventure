26c9cf312b87d589b5ac2420709614bb885fcbd0
migarakavishan <migarakavishan43@gmail.com>
2024-09-21 18:48:23 +0530

    add checkpoint flag

--- lib/components/checkpoint.dart
+++ lib/components/checkpoint.dart
@@ -4,14 +4,17 @@ import 'dart:async';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:pixel_adventure/components/player.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 class Checkpoint extends SpriteAnimationComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, CollisionCallbacks {
   Checkpoint({
     position,
     size,
   }) : super(position: position, size: size);
+
+  bool reachedCheckpoint = false;
 
   @override
   FutureOr<void> onLoad() {
@@ -19,6 +22,7 @@ class Checkpoint extends SpriteAnimationComponent
     add(RectangleHitbox(
       position: Vector2(18, 56),
       size: Vector2(12, 8),
+      collisionType: CollisionType.passive,
     ));
     animation = SpriteAnimation.fromFrameData(
         game.images
@@ -29,5 +33,37 @@ class Checkpoint extends SpriteAnimationComponent
           textureSize: Vector2.all(64),
         ));
     return super.onLoad();
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is Player && !reachedCheckpoint) _reachedCheckpoint();
+    super.onCollision(intersectionPoints, other);
+  }
+
+  void _reachedCheckpoint() {
+    reachedCheckpoint = true;
+    animation = SpriteAnimation.fromFrameData(
+        game.images.fromCache(
+            'Items/Checkpoints/Checkpoint/Checkpoint (Flag Out) (64x64).png'),
+        SpriteAnimationData.sequenced(
+          amount: 26,
+          stepTime: 0.05,
+          textureSize: Vector2.all(64),
+          loop: false,
+        ));
+
+    const flagDuration = Duration(milliseconds: 1300);
+    Future.delayed(flagDuration, () {
+      animation = SpriteAnimation.fromFrameData(
+        game.images.fromCache(
+            'Items/Checkpoints/Checkpoint/Checkpoint (Flag Idle)(64x64).png'),
+        SpriteAnimationData.sequenced(
+          amount: 10,
+          stepTime: 0.05,
+          textureSize: Vector2.all(64),
+          
+        ));
+    });
   }
 }
