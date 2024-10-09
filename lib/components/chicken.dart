5786a03b95426defbe756ae8d5557203cfb0d130
migarakavishan <migarakavishan43@gmail.com>
2024-10-09 23:52:12 +0530

    "Updated Chicken component to use SpriteAnimation and added it to Level component"

--- lib/components/chicken.dart
+++ lib/components/chicken.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
 
-class Chicken extends SpriteAnimationComponent {
+enum State { idle, run, hit }
+
+class Chicken extends SpriteAnimationComponent with HasGameRef<PixelAdventure> {
   final double offNeg;
   final double offPos;
   Chicken({
@@ -12,9 +15,41 @@ class Chicken extends SpriteAnimationComponent {
     this.offPos = 0,
   });
 
+  static const stepTime = 0.05;
+  final textureSize = Vector2(32, 34);
+
+  late final SpriteAnimation _idleAnimation;
+  late final SpriteAnimation _runAnimation;
+  late final SpriteAnimation _hitAnimation;
+
   @override
   FutureOr<void> onLoad() {
     debugMode = true;
+    _loadAllAnimations();
     return super.onLoad();
+  }
+
+  void _loadAllAnimations() {
+    _idleAnimation = _spriteAnimation('Idle', 13);
+    _runAnimation = _spriteAnimation('Run', 14);
+    _hitAnimation = _spriteAnimation('Hit', 5)..loop = false;
+
+    animations = {
+      State.idle: _idleAnimation,
+      State.run: _runAnimation,
+      State.hit: _hitAnimation,
+    };
+
+    current = State.idle;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+        game.images.fromCache('Enemies/Chicken/$state (32x34).png'),
+        SpriteAnimationData.sequenced(
+          amount: amount,
+          stepTime: stepTime,
+          textureSize: textureSize,
+        ));
   }
 }
